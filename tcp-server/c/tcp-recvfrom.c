1b3c14cb51004b7c93cf41c7ff3dbf349d7dbac8
falsy <me@falsy.me>
2020-05-11 05:24:37 +0900

    불필요한 코드 제거

--- tcp-server/c/tcp-recvfrom.c
+++ tcp-server/c/tcp-recvfrom.c
@@ -15,10 +15,6 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char message[BUF_SIZE];
 
-    // Only this line has been changed. Everything is same.
-    // char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("In socket");
@@ -52,102 +48,11 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE);
         }
 
-        // char buffer[30000] = {0};
         valread = read(new_socket , message, BUF_SIZE);
         printf("%s\n", message );
-        // write(new_socket , hello , strlen(hello));
-        // printf("------------------Hello message sent-------------------");
         close(new_socket);
     }
     return 0;
 }
 
 // 참고 https://medium.com/from-the-scratch/http-server-what-do-you-need-to-know-to-build-a-simple-http-server-from-scratch-d1ef8945e4fa
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-// #include <arpa/inet.h>
-// #include <sys/socket.h>
-
-// #define PORT 6666
-// #define BUF_SIZE 5
-// void error_handling(char *message);
-
-// int main(int argc, char *argv[]) {
-//   int serv_sock, new_sock;
-//   char message[BUF_SIZE];
-//   // socklen_t serv_adr_sz;
-//   int str_len;
-  
-//   struct sockaddr_in serv_adr, clnt_adr;
-
-//   char msg[] = "start";
-//   printf("%s", msg);
-
-//   if(argc != 2) {
-//     printf("usage: %s <port>\n", argv[0]);
-//     exit(1);
-//   } else {
-//     char msg[] = "start OK";
-//     printf("%s", msg);
-//   }
-
-//   serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-//   if(serv_sock == -1) {
-//     error_handling("TCP socket creation error");
-//   } else {
-//     char msg[] = "socket OK";
-//     printf("%s", msg);
-//   }
-
-//   memset(&serv_adr, 0, sizeof(serv_adr));
-//   serv_adr.sin_family = AF_INET;
-//   serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-//   serv_adr.sin_port = htons(PORT);
-  
-//   if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1) {
-//     error_handling("bind() error");
-//   } else {
-//     char msg[] = "bind OK";
-//     printf("%s", msg);
-//   }
-
-//   if(listen(serv_sock, 2) == -1) {
-//     error_handling("listen() error");
-//   } else {
-//     char msg[] = "listen OK";
-//     printf("%s", msg);
-//   }
-
-//   printf("aa");
-
-//   // clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, sizeof(clnt_adr));
-
-//   // printf("%d", clnt_sock);
-
-//   while(1) {
-//     // serv_adr_sz = sizeof(serv_adr);
-//     int serv_adr_sz = sizeof(serv_adr);
-//     new_sock = accept(serv_sock, (struct sockaddr*)&serv_adr, (socklen_t*)&serv_adr_sz);
-
-//     printf("%d", new_sock);
-
-//     if(new_sock < 0) {
-//       printf("%s\n", "none accept");
-//     } else {
-//       str_len = read(new_sock, message, BUF_SIZE);
-//       printf("%s\n", message);
-//     };
-//   }
-
-//   // closesocket(serv_sock);
-//   // close(serv_sock);
-//   // return 0;
-// }
-
-// void error_handling(char *message) {
-//   fputs(message, stderr);
-//   exit(1);
-// }
--- tcp-server/c/tcp-sendto.c
+++ tcp-server/c/tcp-sendto.c
@@ -26,7 +26,6 @@ int main(int argc, char const *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
     
-    // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
     {
         printf("\nInvalid address/ Address not supported \n");
@@ -49,69 +48,3 @@ int main(int argc, char const *argv[])
 }
 
 // 참고 https://medium.com/from-the-scratch/http-server-what-do-you-need-to-know-to-build-a-simple-http-server-from-scratch-d1ef8945e4fa
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-// #include <arpa/inet.h>
-// #include <sys/socket.h>
-
-// #define BUF_SIZE 50
-// void error_handling(char *message);
-
-// int main(int argc, char *argv[]) {
-//   int sock;
-//   char message[BUF_SIZE];
-//   int str_len;
-//   socklen_t adr_sz;
-
-//   struct sockaddr_in serv_adr, from_adr;
-
-//   if(argc != 3) {
-//     printf("Usage : %s <IP> <port>\n", argv[0]);
-//     exit(1);
-//   }
-
-//   sock = socket(PF_INET, SOCK_STREAM, 0);
-//   if(sock == -1) {
-//     char msg1[] = "socket() error";
-//     error_handling(msg1);
-//   } else {
-//     char msg[] = "socket OK";
-//     printf("%s", msg);
-//   }
-
-//   memset(&serv_adr, 0, sizeof(serv_adr));
-//   serv_adr.sin_family = AF_INET;
-//   serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-//   serv_adr.sin_port = htons(atoi(argv[2]));
-
-//   if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr) == -1)) {
-//     char msg1[] = "connect error";
-//     error_handling(msg1);
-//   } else {
-//     char msg1[] = "connect OK";
-//     printf("%s", msg1);
-//   }
-
-//   while (1) {
-//     char msg[] = "Insert message(q to quit): ";
-//     fputs(msg, stdout);
-//     fgets(message, sizeof(message), stdin);
-//     char breakString[] = "q\n";
-//     if(!strcmp(message, breakString)) break;
-
-//     send(sock, message, sizeof(message), 0);
-//     printf("Send Message: %s", message);
-//   }
-
-//   close(sock);
-//   return 0;
-// }
-
-// void error_handling(char *message) {
-//   fputs(message, stderr);
-//   fputc('\n', stderr);
-//   exit(1);
-// }
