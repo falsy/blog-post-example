b03c2a902643228a108ba98bc34dd4a09843cb9c
Do it for fun <me@falsy.me>
2023-08-07 07:52:30 +0900

    바텀시트 코드 추가

---
+++ android-for-frontend/bottom-sheet/android/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ModalBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view: View = inflater.inflate(R.layout.fragment_bottomsheet, container, false)
+        return view
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        val modalBottomSheet = ModalBottomSheet()
+
+        button.setOnClickListener {
+            modalBottomSheet.show(supportFragmentManager, "Open Bottom Sheet")
+        }
+    }
+}
