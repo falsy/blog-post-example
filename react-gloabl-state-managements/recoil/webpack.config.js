2a12bd308f07bb199f39b96c91dd38ee690ef03e
CheolJe <me@falsy.me>
2024-09-28 21:24:41 +0900

    feat: 기본 환경 구성 코드 추가

---
+++ react-gloabl-state-managements/recoil/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 9999,
+  },
+}
