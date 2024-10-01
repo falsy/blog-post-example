60820c5d90c4dad806f7c3e81b75e8f66706195d
CheolJe <me@falsy.me>
2024-10-01 22:35:35 +0900

    feat: lazy 샘플 코드 추가

--- basic-react/src/App.tsx
+++ basic-react/src/App.tsx
@@ -1,12 +1,26 @@
+import { lazy, Suspense, useState } from "react"
 import Portal from "./Portal"
 
+const LazyComponent = lazy(() => import("./LazyComponent"))
+
 export default function App() {
+  const [load, setLoad] = useState(false)
+
   return (
     <div>
       <h1>Basic React</h1>
       <section>
         <h2>Portal</h2>
         <Portal />
+      </section>
+      <section>
+        <h2>Lazy</h2>
+        <button onClick={() => setLoad(true)}>Load</button>
+        {load && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyComponent />
+          </Suspense>
+        )}
       </section>
     </div>
   )
