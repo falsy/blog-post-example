08b21a8ffde64a42ab57c4d31d4fa2ea499b1e32
CheolJe <me@falsy.me>
2024-10-06 11:15:34 +0900

    feat: 공백 수정

--- macOS-project/dockmenutest/dockmenutest/dockmenutestApp.swift
+++ macOS-project/dockmenutest/dockmenutest/dockmenutestApp.swift
@@ -9,25 +9,25 @@ import SwiftUI
 
 @main
 struct TestApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-    var body: some Scene {
-        WindowGroup {
-            Text("Hello, World!")
-        }
+  var body: some Scene {
+    WindowGroup {
+      Text("Hello, World!")
     }
+  }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDockMenu(_ sender: NSApplication) -> NSMenu? {
-        let dockMenu = NSMenu(title: "MyMenu")
-        let myMenuItem = NSMenuItem(title: "My Item 1", action: #selector(self.menuItemAction), keyEquivalent: "")
-        dockMenu.addItem(myMenuItem)
-        
-      return dockMenu
-    }
+  func applicationDockMenu(_ sender: NSApplication) -> NSMenu? {
+    let dockMenu = NSMenu(title: "MyMenu")
+    let myMenuItem = NSMenuItem(title: "My Item 1", action: #selector(self.menuItemAction), keyEquivalent: "")
+    dockMenu.addItem(myMenuItem)
+      
+    return dockMenu
+  }
 
-    @objc func menuItemAction() {
-        print("Menu item clicked")
-    }
+  @objc func menuItemAction() {
+    print("Menu item clicked")
+  }
 }
